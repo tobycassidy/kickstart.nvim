b19150dc17e1d3e988c0a827e1c4feb0d08fe69c
tobycassidy <76525529+tobycassidy@users.noreply.github.com>
2025-08-23 14:43:32 +0100

    adjust element size, remove pandas and conditionals

--- lua/custom/plugins/python_debugger.lua
+++ lua/custom/plugins/python_debugger.lua
@@ -4,8 +4,8 @@ return {
   {
     'mfussenegger/nvim-dap',
     dependencies = {
-      'nvim-neotest/nvim-nio',
       'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
       'mfussenegger/nvim-dap-python',
       'theHamsta/nvim-dap-virtual-text',
     },
@@ -14,155 +14,111 @@ return {
       local dapui = require 'dapui'
       local dap_python = require 'dap-python'
 
-      local function setup_dap()
-        dapui.setup {
-          layouts = {
-            {
-              elements = {
-                -- Grouping these three makes sense for state inspection
-                { id = 'scopes', size = 0.6 }, -- Give scopes the most space in this panel
-                { id = 'stacks', size = 0.2 },
-                { id = 'watches', size = 0.2 },
-              },
-              size = 40,
-              position = 'left',
-            },
-            {
-              elements = { 'repl', 'console' },
-              size = 0.25,
-              position = 'bottom',
-            },
-            {
-              elements = { 'breakpoints' },
-              size = 20,
-              position = 'right',
-            },
+      dap.adapters.python = {
+        type = 'executable',
+        command = vim.fn.stdpath 'data' .. '/mason/packages/debugpy/venv/bin/python',
+        args = { '-m', 'debugpy.adapter' },
+      }
+
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          console = 'internalConsole',
+          pythonPath = function()
+            return vim.fn.exepath 'python3' or vim.fn.exepath 'python'
+          end,
+        },
+      }
+
+      dapui.setup {
+        controls = {
+          icons = {
+            pause = '',
+            play = '',
+            step_into = '',
+            step_over = '',
+            step_out = '',
+            step_back = '',
+            run_last = '↻',
+            terminate = '',
           },
-          render = {
-            max_value_lines = 100, -- Show more lines for expanded values
-          },
-          extensions = {
-            pandas_visualizer = {
-              render = function(dapui_node)
-                if dapui_node.variable.type ~= 'pandas.core.frame.DataFrame' then
-                  return nil
-                end
-
-                local expression = string.format(
-                  "print('Shape: ' .. repr(%s.shape) .. '\\n\\n' .. %s.head(10).to_markdown())",
-                  dapui_node.variable.name,
-                  dapui_node.variable.name
-                )
-                local result, bufnr = dap.repl.execute(expression)
-
-                if result.success then
-                  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-                  vim.api.nvim_buf_delete(bufnr, { force = true })
-                  return lines
-                else
-                  return { 'Error fetching DataFrame view' }
-                end
-              end,
+        },
+        layouts = {
+          {
+            elements = {
+              { id = 'scopes', size = 0.55 },
+              { id = 'stacks', size = 0.05 },
+              { id = 'breakpoints', size = 0.05 },
+              { id = 'console', size = 0.05 },
+              { id = 'watches', size = 0.3 },
             },
+            size = 0.5,
+            position = 'right',
           },
-        }
+          { elements = { { id = 'repl' } }, size = 0.33, position = 'bottom' },
+        },
+        render = { max_value_lines = 100 },
+        extensions = {},
+      }
 
-        require('nvim-dap-virtual-text').setup {
-          commented = true,
-        }
+      require('nvim-dap-virtual-text').setup { commented = true }
 
-        dap_python.setup 'python'
+      vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticSignError' })
+      vim.fn.sign_define('DapBreakpointConditional', { text = 'C', texthl = 'DiagnosticSignWarn' })
+      vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DiagnosticSignError' })
+      vim.fn.sign_define('DapStopped', { text = '', texthl = 'DiagnosticSignWarn', linehl = 'Visual' })
 
-        vim.fn.sign_define('DapBreakpoint', {
-          text = '',
-          texthl = 'DiagnosticSignError',
-          linehl = '',
-          numhl = '',
-        })
-
-        vim.fn.sign_define('DapBreakpointRejected', {
-          text = '',
-          texthl = 'DiagnosticSignError',
-          linehl = '',
-          numhl = '',
-        })
-
-        vim.fn.sign_define('DapStopped', {
-          text = '',
-          texthl = 'DiagnosticSignWarn',
-          linehl = 'Visual',
-          numhl = 'DiagnosticSignWarn',
-        })
-
-        dap.listeners.after.event_initialized['dapui_config'] = function()
-          dapui.open()
-        end
-
-        dap.listeners.before.event_terminated['dapui_config'] = function()
-          dapui.close()
-        end
-        dap.listeners.before.event_exited['dapui_config'] = function()
-          dapui.close()
-        end
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
       end
 
       local function map(mode, keys, func, desc)
         vim.keymap.set(mode, keys, func, { desc = desc })
       end
 
-      setup_dap()
+      map('n', '<leader>dbb', dap.toggle_breakpoint, '[D]ebug toggle [B]reakpoint (Basic)')
 
-      map('n', '<leader>db', function()
-        dap.toggle_breakpoint()
-      end, '[D]ebug toggle [B]reakpoint')
+      map('n', '<leader>dbc', function()
+        local condition = vim.fn.input 'Breakpoint Condition: '
+        if condition and condition ~= '' then
+          dap.toggle_breakpoint(condition)
+        end
+      end, '[D]ebug toggle [B]reakpoint (Conditional)')
 
       map('n', '<leader>dr', function()
         local dir = vim.fn.input('Run debugger from directory: ', vim.fn.getcwd(), 'dir')
-        if dir ~= '' then
-          dap_python.test_runner = 'pytest'
-          dap.configurations.python = {
-            {
-              type = 'python',
-              request = 'launch',
-              name = 'Launch with custom dir',
-              program = '${file}',
-              pythonPath = function()
-                return vim.fn.exepath 'python'
-              end,
-              cwd = dir,
-            },
+        if dir and dir ~= '' then
+          local config = {
+            type = 'python',
+            request = 'launch',
+            name = 'Launch with custom dir',
+            program = '${file}',
+            cwd = dir,
+            console = 'internalConsole',
+            pythonPath = function()
+              return vim.fn.exepath 'python3' or vim.fn.exepath 'python'
+            end,
           }
-          dap.continue()
+          dap_python.test_runner = 'pytest'
+          dap.run(config)
         end
       end, '[D]ebug [R]un from directory')
 
-      map('n', '<leader>dc', function()
-        dap.continue()
-      end, '[D]ebug [C]ontinue')
-
-      map('n', '<leader>do', function()
-        dap.step_over()
-      end, '[D]ebug Step [O]ver')
-
-      map('n', '<leader>di', function()
-        dap.step_into()
-      end, '[D]ebug Step [I]nto')
-
-      map('n', '<leader>dO', function()
-        dap.step_out()
-      end, '[D]ebug Step [O]ut')
-
-      map('n', '<leader>dq', function()
-        dapui.close()
-        dap.terminate()
-        dap.configurations = {}
-        dap.adapters = {}
-        setup_dap()
-      end, '[D]ebug [Q]uit')
-
-      map('n', '<leader>du', function()
-        dapui.toggle()
-      end, '[D]ebug toggle [U]I')
+      map('n', '<leader>dc', dap.continue, '[D]ebug [C]ontinue')
+      map('n', '<leader>do', dap.step_over, '[D]ebug Step [O]ver')
+      map('n', '<leader>di', dap.step_into, '[D]ebug Step [I]nto')
+      map('n', '<leader>dO', dap.step_out, '[D]ebug Step [O]ut')
+      map('n', '<leader>dq', dap.terminate, '[D]ebug [Q]uit')
+      map('n', '<leader>du', dapui.toggle, '[D]ebug toggle [U]I')
     end,
   },
 }
