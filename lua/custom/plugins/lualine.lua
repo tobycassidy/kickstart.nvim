adfe56c49956c752d20eca07c355abe83689cec1
tobycassidy <76525529+tobycassidy@users.noreply.github.com>
2025-08-23 15:53:45 +0100

    status bar

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,25 @@
+-- lua/custom/plugins/lualine.lua
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        -- You can change the theme here to any of the supported ones:
+        -- 'auto', 'tokyonight', 'dracula', 'powerline_dark', etc.
+        theme = 'nordic',
+        icons_enabled = true,
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
+  end,
+}
